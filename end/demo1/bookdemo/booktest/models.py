c2e4e9d4558ae2153d26e146dbcb02ebbbe8f9b9
wjw0321 <1815095781@qq.com>
2020-02-11 17:57:44 +0800

    all

--- end/demo1/bookdemo/booktest/models.py
+++ end/demo1/bookdemo/booktest/models.py
@@ -7,9 +7,12 @@ class Book(models.Model):
     """
     book继承了Model类  因为该类具有操控数据库的功能
     """
-    title = models.CharField(max_length=20)
+    title = models.CharField(max_length=20, )
     pub_date = models.DateField(default="1983-06-01")
     price = models.FloatField(default=0)
+
+    def __str__(self):
+        return self.title
 
 
 class Hero(models.Model):
@@ -21,3 +24,6 @@ class Hero(models.Model):
     content = models.CharField(max_length=100)
 
     book = models.ForeignKey(Book,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
