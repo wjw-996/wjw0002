08bcd1a9f42c3ef52504378f3944a22c83fc84b7
wjw0321 <1815095781@qq.com>
2020-02-11 16:04:21 +0800

    all

--- end/demo1/bookdemo/booktest/models.py
+++ end/demo1/bookdemo/booktest/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Book(models.Model):
+    """
+    book继承了Model类  因为该类具有操控数据库的功能
+    """
+    title = models.CharField(max_length=20)
+    pub_date = models.DateField(default="1983-06-01")
+    price = models.FloatField(default=0)
+
+
+class Hero(models.Model):
+    """
+
+    """
+    name = models.CharField(max_length=20)
+    gender = models.CharField(max_length=6, choices=(('male', '男'), ('female', '女')), default='male')
+    content = models.CharField(max_length=100)
+
+    book = models.ForeignKey(Book,on_delete=models.CASCADE)
