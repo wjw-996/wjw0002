06640cbaf210ef03d0d92c6ecea82701b72b3f38
wjw0321 <1815095781@qq.com>
2020-02-16 17:18:35 +0800

    投票小程序数据库完成

---
+++ end/demo1/bookdemo/polls/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+# Create your models here.
+class Headline(models.Model):
+    title = models.CharField(max_length=30)
+
+
+class Option(models.Model):
+    content = models.CharField(max_length=20)
+    num = models.CharField(max_length=10, default='0')
+    headline = models.ForeignKey(Headline, on_delete=models.CASCADE)
---
+++ end/demo1/bookdemo/polls/urls.py
@@ -0,0 +1,11 @@
+# 每一个路由文件必须编写路由数组 urlpatterns
+from django.conf.urls import url
+from . import views
+
+# 从视图文件views中导入所有视图函数
+app_name = "polls"
+
+# 2、将路由与视图函数绑定
+urlpatterns = [
+    # url(r'^about/$', views.about, name='about'),
+]
