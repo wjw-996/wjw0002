74d59cc4365bed61dfe91a35e33a196b9339771f
wjw0321 <1815095781@qq.com>
2020-02-19 15:21:38 +0800

    博客数据模型

--- end/dame2/blog/blogapp/models.py
+++ end/dame2/blog/blogapp/models.py
@@ -20,7 +20,7 @@ class Article(models.Model):
     create_time = models.DateTimeField(auto_now_add=True, verbose_name="发表时间")
     update_time = models.DateTimeField(auto_now=True, verbose_name="修改时间")
     author = models.CharField(max_length=20, default="佚名", verbose_name="作者")
-    views = models.CharField(default=0, verbose_name="浏览量")
+    views = models.PositiveIntegerField(default=0, verbose_name="浏览量")
     body = models.TextField(verbose_name="正文")
     tags = models.ManyToManyField(Tag)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name="分类")
