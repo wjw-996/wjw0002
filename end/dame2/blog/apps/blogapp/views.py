88a0d5644e9280269f716e600ee7d32592a7fe2f
wjw0321 <1815095781@qq.com>
2020-02-20 17:56:19 +0800

    博客首页正文，分页器

--- end/dame2/blog/apps/blogapp/views.py
+++ end/dame2/blog/apps/blogapp/views.py
@@ -1,12 +1,21 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import *
+from django.core.paginator import Paginator, Page
+
 
 # Create your views here.
 
 def index(request):
     ads = Ads.objects.all()
-    return render(request,"index.html",locals())
+    category = Category.objects.all()
+    article = Article.objects.all()
+    tag = Tag.objects.all()
+
+    paginator = Paginator(article, 2)
+    num = request.GET.get("pagenumber", 1)
+    page = paginator.get_page(num)
+    return render(request, "index.html", {"ads": ads, "page": page})
     # return HttpResponse("首页")
 
 
