eb6e3c10ce323949f4f8f2da78cdba72ca35b473
wjw0321 <1815095781@qq.com>
2020-02-20 15:00:41 +0800

    博客首页轮播图

--- end/dame2/blog/apps/blogapp/views.py
+++ end/dame2/blog/apps/blogapp/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+from .models import *
 
 # Create your views here.
+
+def index(request):
+    ads = Ads.objects.all()
+    return render(request,"index.html",locals())
+    # return HttpResponse("首页")
+
+
+def detail(request, articleid):
+    return render(request, "single.html")
+    # return HttpResponse("detail")
+
+
+def contact(request):
+    return render(request, "contact.html")
+    # return HttpResponse("contact")
+
+
+def favicon(request):
+    # 如果获取logo则重定向到一个图片资源
+    return redirect(to="/static/favicon.ico")
--- end/dame2/blog/blog/settings.py
+++ end/dame2/blog/blog/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 X_FRAME_OPTIONS = "sameorigin"
 
